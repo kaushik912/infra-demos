a786900ec6fe2052f127d2b2b56a2a22d2357516
kaushik912 <kaushik.1986@gmail.com>
2026-09-25 17:57:02 +0000

    Revert "Remove newrelic-demos, moved to standalone repo"
    This reverts commit 78f6abea17aae3ca6563951e8c2f5914105621a4.

---
+++ newrelic-demos/src/main/java/com/demo/newrelic/NewrelicDemosApplication.java
@@ -0,0 +1,13 @@
+package com.demo.newrelic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewrelicDemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewrelicDemosApplication.class, args);
+	}
+
+}
